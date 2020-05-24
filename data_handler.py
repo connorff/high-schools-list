0499889dcd657007be80b03c2b78fb753a2e050b
Connor Fogarty <connorflynnfogarty@gmail.com>
2020-05-24 01:25:35 -0500

    Init - able to read public and private data and produce list of all schools in both

---
+++ data_handler.py
@@ -0,0 +1,113 @@
+import csv
+import os
+
+class Data_Handler:
+    file_types = ("csv")
+
+    def __init__(self, file_name, school_name_column, school_city_column, school_state_column, school_level_column, min_grade_size, grade_columns, good_levels):
+        if not file_name.endswith(self.file_types):
+            raise ValueError(f"File must be of types: {self.file_types}")
+        
+        if not self.file_exists(file_name):
+            raise ValueError("That file does not exist")
+
+        self.fn = file_name
+
+        self.name = school_name_column
+        self.city = school_city_column
+        self.state = school_state_column
+        self.level = school_level_column
+        self.min_grade_size = min_grade_size
+        self.grade_columns = grade_columns
+        self.good_levels = good_levels
+
+    
+    def file_exists(self, file_name):
+        return os.path.isfile(file_name)
+
+
+    def get_school_names(self):
+        school_data = []
+
+        with open(self.fn, errors='ignore') as f:
+            reader = csv.reader(f, delimiter=',')
+            begin = True
+
+            columns = [
+                self.name,
+                self.city,
+                self.state,
+                self.level,
+            ]
+
+            # if there is no grade limiting set, do not add to column list
+            if self.grade_columns != None:
+                columns.extend([self.grade_columns[0],
+                                self.grade_columns[1],])
+
+            column_indexes = []
+
+            for row in reader:
+                if begin:
+                    # if column name not found in sheet, raise error
+                    for x in range(len(columns)):
+                        col = columns[x]
+
+                        index = row.index(col)
+                        
+                        # index not found
+                        if index == -1:
+                            raise ValueError(f"{col} in spreadsheet not valid")
+                            break
+                            
+                        # store index of column name
+                        column_indexes.append(index)
+
+                    begin = False
+                else:
+                    min_grade, max_grade = None, None
+
+                    if self.grade_columns != None:
+                        min_grade = self.convert_grade(row[column_indexes[4]])
+                        max_grade = self.convert_grade(row[column_indexes[5]])
+
+                    if self.check_school(row[column_indexes[3]], min_grade, max_grade):
+                        school_name = row[column_indexes[0]]
+                        school_city = row[column_indexes[1]]
+                        school_state = row[column_indexes[2]]
+
+                        school_data.append([school_name, school_city, school_state])
+
+        return school_data
+
+    # checks if school is valid:
+    #   - valid level
+    #   - valid grade range
+    def check_school(self, level, min_grade = None, max_grade = None):
+        if level not in self.good_levels:
+            return False
+        if min_grade != None:
+            min_grade = self.convert_grade(min_grade)
+            max_grade = self.convert_grade(max_grade)
+
+            if max_grade - min_grade < self.min_grade_size:
+                return False
+
+        return True
+
+
+    # converts grade into integer equivalent
+    def convert_grade(self, grade):
+        # checks if grade has string version in object
+        if grade in list(self.grade_columns[2].keys()):
+            grade = self.grade_columns[2][grade]
+        else:
+            try:
+                grade = int(grade)
+            except ValueError:
+                raise ValueError(f"{grade} cannot be converted to integer")
+        return grade
+
+
+    def get_school_city(self, row):
+        city_index = 1
---
+++ main.py
@@ -0,0 +1,51 @@
+from data_handler import Data_Handler
+
+# how many grades a high school must have to be added
+# for example, a high school with only freshman may not be suitable
+min_grade_size = 3
+
+file_data = {
+    "public": {
+        "file_name": "files/Public_Universe_Survey.csv",
+        "name_column": "SCH_NAME",
+        "city_column": "MCITY",
+        "state_column": "STATENAME",
+        "level_column": "LEVEL",
+        "good_levels": [
+            "High",
+        ],
+        # lowest grade offered,
+        # highest grade offered,
+        # object of string grades and their integer representation
+        "grade_columns": [
+            "GSLO",
+            "GSHI",
+            {
+                "KG": 0,
+                "PK": -1,
+                "N": 0,
+                # missing
+                "M": 0,
+                # ungraded
+                "UG": 0,
+                # adult education
+                "AE": 0,
+            }
+        ]
+    },
+    "private": {
+        "file_name": "files/Private_Universe_Survey.csv",
+        "name_column": "pinst",
+        "city_column": "pcity",
+        "state_column": "pstabb",
+        "level_column": "level",
+        "good_levels": [
+            "2",
+        ],
+        "grade_columns": None
+    }
+}
+
+if __name__ == "__main__":
+    for key in list(file_data.keys()):
+        dh = Data_Handler(file_data[key]["file_name"], file_data[key]["name_column"], file_data[key]["city_column"], file_data[key]["state_column"], file_data[key]["level_column"], min_grade_size, file_data[key]["grade_columns"], file_data[key]["good_levels"])
